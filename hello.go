cb9eee16e59ae3709ce86e678d26623b3ac8e289
Renan Maragon Ferreira da Cunha <renan.cunha@hibnu.local>
2018-04-27 14:23:27 -0300

    inicial com tudo dentro do main, fluxo if e switch

--- hello.go
+++ hello.go
@@ -1,3 +1,5 @@
+// git ls-files (para saber qual arquivos o git está controlando)
+
 package main
 
 // para build:  go build hello.go
@@ -9,30 +11,46 @@ import "reflect"
 
 func main() {
 	nome := "Douglas"
-	idade:= 24
+	idade := 24
 
 	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
-	
-	versao:= 1.1
+
+	versao := 1.1
 	fmt.Println("Olá sr.!", nome, "sua idade é", idade)
-	fmt.Println	("Este programa está na versão", versao)
-	
+	fmt.Println("Este programa está na versão", versao)
+
 	fmt.Println("1- Iniiaf Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
 
 	var comando int
-	var comandos int
+
 	// Sacnf precisa do modificar (%d), varial
-	fmt.Scanf ("%d", &comando)
+	//	fmt.Scanf ("%d", &comando)
 	// Scan é mais inteligente e não precisa do modificador
-	fmt.Scan(&comandos)
-
+	fmt.Scan(&comando)
 	fmt.Println("O endereço da variável comando é", &comando)
-	fmt.Println("O endereço da variável comando é", &comandos)
-
 	fmt.Println("o comando escolhido foi", comando)
-}
 
-//
-// git ls-files (para saber qual arquivos o git está controlando)
+	//	if comando == 1 {
+	//		fmt.Println("Monitando...")
+	//	} else if comando == 2 {
+	//		fmt.Println("Exibindo Logs...")
+	//	} else if comando == 0 {
+	//		fmt.Println("Saindo do Programa")
+	//	} else {
+	//		fmt.Println("Não conheço este comando")
+	//	}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Log...")
+	case 3:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conhço este comando")
+	}
+
+}
