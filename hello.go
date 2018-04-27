0930cffd7e97bf7b02ff7c0b1757b3fbf6f338c2
Renan Marangon <renan.cunha@hiplatform.com>
2018-04-27 16:04:35 -0300

    Introdução inicial às funções

--- hello.go
+++ hello.go
@@ -1,56 +1,49 @@
-// git ls-files (para saber qual arquivos o git está controlando)
-
 package main
 
-// para build:  go build hello.go
-// para executar: ./hello
-// ou para ir direto: go run hello.go
-
 import "fmt"
-import "reflect"
+import "os"
 
 func main() {
-	nome := "Douglas"
-	idade := 24
 
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+	exibeIntrodução()
+	exibeMenu()
 
-	versao := 1.1
-	fmt.Println("Olá sr.!", nome, "sua idade é", idade)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1- Iniiaf Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-
-	// Sacnf precisa do modificar (%d), varial
-	//	fmt.Scanf ("%d", &comando)
-	// Scan é mais inteligente e não precisa do modificador
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da variável comando é", &comando)
-	fmt.Println("o comando escolhido foi", comando)
-
-	//	if comando == 1 {
-	//		fmt.Println("Monitando...")
-	//	} else if comando == 2 {
-	//		fmt.Println("Exibindo Logs...")
-	//	} else if comando == 0 {
-	//		fmt.Println("Saindo do Programa")
-	//	} else {
-	//		fmt.Println("Não conheço este comando")
-	//	}
+	comando := leComando()
 
 	switch comando {
 	case 1:
 		fmt.Println("Monitorando...")
 	case 2:
 		fmt.Println("Exibindo Log...")
-	case 3:
+	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
-		fmt.Println("Não conhço este comando")
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
 
+}
+
+func exibeIntrodução() {
+
+	nome := "Gullas"
+	idade := 24
+	versao := 1.1
+	fmt.Println("Olá sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniiaf Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("o comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
