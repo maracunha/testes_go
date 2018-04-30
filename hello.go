1ba22b47c9cb1858c85bdbe1b3f1087748b44015
Renan Marangon <renan.cunha@hiplatform.com>
2018-04-30 17:01:00 -0300

    Array e Slices com exemlos de funcionalidades

--- hello.go
+++ hello.go
@@ -1,49 +1,36 @@
-// _ é um operador de variavel em branco
-	//nome, idade := devolveNomeIdade()    (assim eu uso todas as variáveis)
-	//fmt.Println("Meu nome é", nome)
-	//fmt.Println("Minha idade é", idade)
-
- 	//_, idade := devolveNomeIdade()    // caso não queira usar alguma, é só usar o _ para ficar em branco
-	//fmt.Println("Minha idade é", idade)
-
-// para retornar mais de uma varial, devo colcoar o que será retornado entre ()
-//func devolveNomeIdade() (string, int) {
-//	nome := "Douglas"
-//	idade := 24
-//	return nome, idade
-// for {  (sem passar nenhum parametro, ele fico em loop infinito.. em go não existe while)
-//} 
-
-
-
 package main
 
-import "fmt"
-import "os"
-
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"reflect"
+)
 
 func main() {
 
-	exibeIntrodução()
-	
+	exibeNomes()  // Slice
+	exibeCarros() // Array
+
+	//exibeIntrodução()
+
 	for {
-		exibeMenu()
+		//	exibeMenu()
 
-	comando := leComando()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Log...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
-	}
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Log...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -58,7 +45,7 @@ func exibeIntrodução() {
 }
 
 func exibeMenu() {
-	fmt.Println("1- Iniiaf Monitoramento")
+	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
 }
@@ -77,9 +64,36 @@ func iniciarMonitoramento() {
 	resp, _ := http.Get(site)
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado corretamente!")
-	}else{
-		fmt.Println("Site", site, "está com problema. Status Code:",resp.StatusCode)
+	} else {
+		fmt.Println("Site", site, "está com problema. Status Code:", resp.StatusCode)
 	}
-	
-	
+}
+
+func exibeNomes() {
+	nomes := []string{"Doulgas", "Mario", "Bernado"}
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))                                  // para saber o tipo de "nome"
+	fmt.Println("O meu slice tem", len(nomes), "itens")                 // LEN para saber o tamnaho do array
+	fmt.Println("O meu slice tem capacidade para", cap(nomes), "itens") // CAP para saber a capacidade do array
+
+	fmt.Println()
+	nomes = append(nomes, "Aparecida")
+
+	fmt.Println(nomes)
+	fmt.Println(reflect.TypeOf(nomes))
+	fmt.Println("O meu slice tem", len(nomes), "itens")
+	fmt.Println("O meu slice tem capacidade para", cap(nomes), "itens")
+}
+
+func exibeCarros() {
+	var carros [4]string
+	carros[0] = "chevete"
+	carros[1] = "civic"
+	carros[2] = "celta"
+
+	fmt.Println()
+	fmt.Println(carros)
+	fmt.Println(reflect.TypeOf(carros))
+	fmt.Println("O meu array tem", len(carros), "itens")
+	fmt.Println("O meu array tem capacidade para", cap(carros), "itens")
 }
