b8f0d91501cb3a29edf43ce3ffc7001cf6b64ac6
Renan Marangon <renan.cunha@hiplatform.com>
2018-05-04 15:58:52 -0300

    Abrindo e criando um arquivo, configuranda data e hora em um LOG

--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -16,7 +17,6 @@ const delay = 5
 func main() {
 
 	exibeIntrodução()
-	leSitesDoArquivo()
 
 	for {
 		exibeMenu()
@@ -90,8 +90,10 @@ func testaSite(site string) {
 	}
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado corretamente!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site", site, "está com problema. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -123,4 +125,19 @@ func leSitesDoArquivo() []string {
 	return sites
 }
 
-// "lendo múltiplas linhas, função cortar linhas e espaços, arquivo.close"
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(arquivo)
+
+	// go representa tempo de uma forma diferente (https://golang.org/src/time/format.go)
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+// "Abrindo e criando um arquivo"
